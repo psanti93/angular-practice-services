2265d5d5d06806dbf4cbab269795ac10e773d65c
Paul Santiago <paulsantiago282@gmail.com>
2023-08-10 18:28:38 -0400

    Add example of beginner logging using service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-
+import { LoggingService } from '../logging/logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
@@ -13,6 +13,7 @@ export class NewAccountComponent {
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    const service = new LoggingService();
+    service.logStatusChange(accountStatus)
   }
 }
